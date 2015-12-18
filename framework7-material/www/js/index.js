e207ea975383ad4c62b7b89314d1d9aa9eeb77fc
Feihong Hsu <feihong.hsu@gmail.com>
2015-12-18 11:39:18 -0600

    Add framework7-material example

---
+++ framework7-material/www/js/index.js
@@ -0,0 +1,46 @@
+var myApp = new Framework7({
+  material: true,   // Enable Material theme
+  modalTitle: 'This is a modal',
+})
+
+var $$ = Dom7
+
+var mainView = myApp.addView('.view-main', {})
+
+function initIndexPage() {
+  $$('.alert-button').on('click', function() {
+    myApp.alert('Woo hoo!')
+  })
+
+  var info = {
+    'Cordova version': device.cordova,
+    'Platform': device.platform,
+    'Device model': device.model,
+    'Platform version': device.version,
+    // 'UUID': device.uuid,
+  }
+
+  for (var key in info) {
+    var val = info[key]
+    $$('.device-info-list').append(
+      '<li><div class="item-content"><div class="item-inner"><div class="item-title">' + key + '</div><div class="item-after">' + val + '</div></div></div></li>'
+    )
+  }
+
+  $$('.uuid').html(device.uuid)
+}
+
+
+var isPhoneGap = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+if (isPhoneGap) {
+    document.addEventListener("deviceready", initIndexPage, false)
+} else {
+  var device = {
+    cordova: 'N/A',
+    platform: 'browser',
+    model: 'N/A',
+    version: 'N/A',
+    uuid: 'N/A',
+  }
+  initIndexPage()
+}
