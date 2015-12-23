f5a30393a23675185c6511f8ae4f4891a132cbc1
Feihong <feihong.hsu@gmail.com>
2015-12-22 22:35:59 -0600

    Set window.isMobileDevice inside mobile.js

--- framework7-react/www/js/mobile.js
+++ framework7-react/www/js/mobile.js
@@ -2,5 +2,6 @@
   var ua = navigator.userAgent.toLowerCase()
   if (ua.match('android') || !ua.match('chrome')) {
     document.write('<script type="text/javascript" src="cordova.js"></script>')
+    window.isMobileDevice = true
   }
 })()
